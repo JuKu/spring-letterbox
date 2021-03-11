6cdd9154c817af744195fa63a0886390e86b652a
JuKu <kuenzel.justin@t-online.de>
2021-03-11 23:43:02 +0100

    Initial commit

---
+++ src/main/java/com/jukusoft/letterbox/TsLetterboxApplication.java
@@ -0,0 +1,13 @@
+package com.jukusoft.letterbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsLetterboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsLetterboxApplication.class, args);
+	}
+
+}
